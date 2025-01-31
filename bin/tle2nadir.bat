4e0901aa2eda1625ebbeb177d9fb86a8f63a93bd
Marcin Jasiukowicz <contact@yasiu.pl>
2025-01-31 10:17:12 +0100

    add venv config

--- bin/tle2nadir.bat
+++ bin/tle2nadir.bat
@@ -1,1 +1,27 @@
-C:/Users/mjasiukowicz/AppData/Local/anaconda3/python.exe tle2nadir_gui.py %*
+@echo off
+setlocal
+
+:: Define the virtual environment directory
+set VENV_DIR=venv
+
+:: Check if venv exists, if not, create it
+if not exist %VENV_DIR% (
+    echo Creating virtual environment...
+    python -m venv %VENV_DIR%
+)
+
+:: Activate the virtual environment
+call %VENV_DIR%\Scripts\activate
+
+:: Install dependencies if needed
+echo Installing dependencies...
+pip install --upgrade pip
+pip install -r requirements.txt
+
+:: Run the Python script
+python tle2nadir_gui.py %*
+
+:: Deactivate (optional)
+deactivate
+
+endlocal
--- bin/tle2nadir.sh
+++ bin/tle2nadir.sh
@@ -1,2 +1,24 @@
-## #!/usr/bin/env python
-python tle2nadir.py
+#!/bin/bash
+
+# Define virtual environment directory
+VENV_DIR="venv"
+
+# Check if venv exists, if not, create it
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv $VENV_DIR
+fi
+
+# Activate the virtual environment
+source $VENV_DIR/bin/activate
+
+# Install dependencies if needed
+echo "Installing dependencies..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run the Python script
+python tle2nadir_gui.py "$@"
+
+# Deactivate (optional)
+deactivate
