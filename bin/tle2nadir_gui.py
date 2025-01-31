ef08b0a21b41aa6eca216e038bca81405c93b87b
Marcin Jasiukowicz <contact@yasiu.pl>
2025-01-31 14:28:20 +0100

    add license

--- bin/tle2nadir_gui.py
+++ bin/tle2nadir_gui.py
@@ -7,6 +7,31 @@ from datetime import datetime, timedelta, timezone
 from skyfield.api import EarthSatellite, load
 
 
+"""
+    VTS Timeloop Generator plugin generating Nadir-pointing quaternion from a satellite TLE over a given time period.
+    Copyright (C) 2025  Marcin Jasiukowicz
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+__author__      = "Marcin Jasiukowicz"
+__copyright__   = "Copyright (C) 2025  Marcin Jasiukowicz"
+__license__     = "GPLv3"
+__version__     = "1.0.0"
+__email__       = "contact@yasiu.pl"
+__status__      = "Production"
+
 # Utility Functions
 def normalize(vector):
     """Normalize a vector."""
